782e7a38d90018c33974e208132e86da4174217d
Royyyyy <chenyuhan@RoyyyyydeMBP.attlocal.net>
2024-11-03 20:33:05 -0600

    Update code with environment variables for API key

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // 中间件解析 JSON 数据
 app.use(express.json());
